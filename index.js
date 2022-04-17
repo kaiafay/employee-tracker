0e8552b9636d2fef07466051c6fd5b726dc24e68
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-17 14:58:53 -0700

    create function for main prompts and options

--- index.js
+++ index.js
@@ -4,8 +4,57 @@ const db = require('./db');
 require('console.table');
 
 
-// write prompts: view all departments, view all roles, view all employees, add a department, 
-// add a role, add an employee, and update an employee role
+// function that holds prompts
+const mainPrompts = () => {
+    console.log("Welcome to Employee Tracker!");
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Quit'
+                ]
+            }
+        ])
+        .then(res => {
+            let choice = res.choice;
+            // call function depending on user choice
+            switch (choice) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployee();
+                    break;
+                default:
+                    quit();
+            }
+        })
+};
 
 // function for view all departments
 
@@ -20,3 +69,5 @@ require('console.table');
 // function for add an employee
 
 // function for update an employee role
+
+// function that quits application
