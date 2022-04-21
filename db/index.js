eac96d8f5a4d6bc75f4212f83a14cc8bb279c8ff
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-20 17:50:56 -0700

    add method that finds all employees

--- db/index.js
+++ db/index.js
@@ -20,6 +20,9 @@ class DB {
 
 
     // find all employees method
+    findAllEmployees() {
+        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;");
+    };
 
     // add department method
 
