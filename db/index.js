43e3e351e40fc021f23c426115a6358cec9cbe81
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-23 12:40:12 -0700

    add method that finds all managers

--- db/index.js
+++ db/index.js
@@ -24,6 +24,11 @@ class DB {
         return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;");
     };
 
+    // find all managers method
+    findAllManagers(employeeId) {
+        return this.connection.promise().query("SELECT id, first_name, last_name FROM employee WHERE id != ?", employeeId);
+    };
+
     // add department method
     addDepartment(department) {
         return this.connection.promise().query("INSERT INTO department SET ?", department);
